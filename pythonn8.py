09745db9b80b32f461b089663a76e31e9aa4704c
HRISHIKESH BHAT <83940584+Hrishi34@users.noreply.github.com>
2023-08-10 12:01:04 +0530

    Create pythonn8.py

---
+++ pythonn8.py
@@ -0,0 +1,12 @@
+import math
+
+def compute_sum(limit):
+    total_sum = 0.0
+    for i in range(1, limit + 1):
+        total_sum += 1.0 / math.sqrt(i)
+    return total_sum
+
+# Calculate the sum for a large range (adjust the limit as needed)
+sum_limit = 1000000
+result = compute_sum(sum_limit)
+print(result)
